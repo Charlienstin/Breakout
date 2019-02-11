533f64feaf474e6dca9975aeaf4657eff70e2ee5
岡田崇 <totetumonakuganbaru@gmail.com>
2019-02-11 17:18:19 +0900

    プロジェクト作成

---
+++ Assets/Scripts/NormalScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalScript : MonoBehaviour
+{
+
+    public GameManager gameManager;
+
+    void OnCollisionEnter()
+    {
+        Destroy(gameObject);
+        gameManager.score += 10;
+    }
+
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour　{
+
+    float accel = 100.0f;
+
+    void Update()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.right * Input.GetAxisRaw("Horizontal") * accel, ForceMode.Impulse);
+    }
+}
